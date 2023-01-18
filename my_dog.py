9eff9ac04d294f4b9f10ce9fefac0368a46650dd
Kristiyan Kolev <106335904+kris7iyan99@users.noreply.github.com>
2023-01-18 08:47:26 +0200

    Delete

--- my_dog.py
+++
@@ -1,8 +0,0 @@
-dog_name = input()
-dog_age = float(input())
-dog_bread = input()
-
-if dog_age == 1.3:
-    print(dog_name)
-else:
-    print(f'Hello, this is my dog {dog_name}, he is {dog_age} old! and his bread is {dog_bread}.')
